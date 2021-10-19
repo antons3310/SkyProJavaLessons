5b1703b86c2b91a9f1587ef80b63969f38da6170
antoshka <ashebalkin@diasoft.ru>
2021-10-20 00:01:27 +0300

    Привео структуру проекта в порядок. Не будем пложить много проектов на гитхабе

---
+++ src/ru/ashebalkin/skypro/lesson05/Homework05.java
@@ -0,0 +1,48 @@
+package ru.ashebalkin.skypro.lesson05;
+
+public class Homework05 {
+
+    public static void main(String[] args) {
+        // write your code here
+        test1();
+        test2();
+        test3();
+        test4();
+        test5();
+        test6();
+        test7();
+        test8();
+    }
+
+    public static void test1() {
+
+    }
+
+    public static void test2() {
+
+    }
+
+    public static void test3() {
+
+    }
+
+    public static void test4() {
+
+    }
+
+    public static void test5() {
+
+    }
+
+    public static void test6() {
+
+    }
+
+    public static void test7() {
+
+    }
+
+    public static void test8() {
+
+    }
+}
