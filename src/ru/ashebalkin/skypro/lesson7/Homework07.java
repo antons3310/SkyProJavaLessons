7c2dbdde238e3958d11afd7f2607585636a8f48c
antoshka <ashebalkin@diasoft.ru>
2021-10-28 21:21:32 +0300

    Подготовка для сдачи заданий 7-го урока

---
+++ src/ru/ashebalkin/skypro/lesson7/Homework07.java
@@ -0,0 +1,28 @@
+package ru.ashebalkin.skypro.lesson7;
+
+public class Homework07 {
+
+    public static void main (String [] arr){
+        test1();
+        test2();
+        test3();
+        test4();
+        test5();
+    }
+
+    public static void test1() {
+    }
+
+    public static void test2() {
+    }
+
+    public static void test3() {
+    }
+
+    public static void test4() {
+    }
+
+    public static void test5() {
+    }
+
+}
