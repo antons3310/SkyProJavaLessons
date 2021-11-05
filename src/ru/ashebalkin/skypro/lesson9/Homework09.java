d5f4dfd6e5465c04c14596180bcd59a6a5f14f07
ashebalkin <ashebalkin@diasoft.ru>
2021-11-05 21:28:34 +0300

    Заготовка для сдачи задач 9-го урока

---
+++ src/ru/ashebalkin/skypro/lesson9/Homework09.java
@@ -0,0 +1,16 @@
+package ru.ashebalkin.skypro.lesson9;
+
+public class Homework09 {
+    public static void main (String [] arr){
+        testEasyLevel();
+        testHardLevel();
+    }
+
+    public static void testEasyLevel(){
+
+    }
+
+    public static void testHardLevel(){
+
+    }
+}
